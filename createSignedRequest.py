390a59ed5747056918fa7c28f99d08233f263850
agallo <agallo@gwu.edu>
2019-09-10 19:49:51 -0400

    error checking appears to be working

--- createSignedRequest.py
+++ createSignedRequest.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 
-import ipaddress
-import time
-import subprocess
-import os
 from argparse import ArgumentParser
-import yaml
+import ipaddress
+import os
 import re
+import subprocess
+import time
+import yaml
 
 
 parser = ArgumentParser(description="Generate signed ROA requests for ARIN portal")
@@ -42,27 +42,75 @@ def preProcessPrefixes(prefixList):
     :param prefixList:
     :return: processedPrefixList
     """
-    processedPrefixList = []
-    for entry in prefixList:
-        components = re.split("[/ -]", entry)
-        if len(components) == 2:                                # no mask length option
-            processedPrefixList.append(components[0] + '/' + components[1])
-        else:                                                   # optional masklength
-            processedPrefixList.append(components[0] + '/' + components[1])           # base network
-            processedPrefixList.append(components[0] + '/' + components[4])           # max length prefix
-    validList, invalidList = validatePrefixList(processedPrefixList)
-    if len(invalidList) > 0:
-        print("***Detected {} invalid prefixes.  Bailing....".format(len(invalidList)))
-        for badline in invalidList:
-            print(badline)
-        os._exit(1)
-    return
+    tvallist = []
+    invalidlist = []
+    print("Entering preProcessPrefixes with prefixList = {}".format(prefixList))
+    for index, item in enumerate(prefixList):
+        # remove any whitespace that may be in the entry, can happen especially when mask range is in use
+        # this makes the result of split more predictable
+        item = item.replace(" ", "")
+        # split the line on '/' or '-' to see if we're dealing with a range of masks
+        components = re.split("[/-]", item)
+        if len(components) == 2:  # no range, only address + mask
+            # we need to rebuild the prefix in case there spaces were removed.  in reality, at this point, for
+            # an entry not using the mask range option, 'item' should be good enough, but rebuilding from the
+            # components drives the point home
+            reformattedprefix = components[0] + '/' + components[1]
+            # print("{} {} no mask range".format(index, reformattedprefix))
+            try:
+                if ipaddress.ip_network(reformattedprefix).version == 4:  # we have a valid v4 prefix
+                    # print("got valid v4")
+                    tvallist.append(reformattedprefix)
+                elif ipaddress.ip_network(reformattedprefix).version == 6:  # we have a valid v6 prefix
+                    # print("got valid v6")
+                    # need to use the output of ipaddress.ip_network because that compresses the v6 addr
+                    # (removes leading zeros), which the ARIN portal doesn't understand
+                    tvallist.append(str(ipaddress.ip_network(reformattedprefix)).upper())
+            except ValueError:  # what ever entry is, it isn't valid
+                # print("neither v4 nor v6")
+                invalidlist.append(item)
+        # this section is a bit more complicated- for networks where the mask range option is used
+        # eg: 172.16.0.0/16-18, we need to temporarily deconstruct the entry, ensure that both the
+        # base network (in this example 172.16.0.0/16) AND the prefix with the max length
+        # (in this example 172.16.0.0/18) are both valid.
+        # we then have to reconstruct the string from the validated components (especially in the case of v6, where
+        # leading zeros need to be removed)
+        if len(components) == 3:  # range option being used, we have address, base mask + max length
+            # print("{} {} using mask range".format(index, item))
+            basenetwork = components[0] + '/' + components[1]
+            maxlennetwork = components[0] + '/' + components[2]
+            reformattedprefixrange = components[0] + '/' + components[1] + '-' + components[2]
+            # first, validate the base network
+            try:
+                if ipaddress.ip_network(basenetwork).version == 4:
+                    # print("got valid v4")
+                    if ipaddress.ip_network(
+                            maxlennetwork).version == 4:  # checking that the prefix with max length mask is valid
+                        # print("max len v4 is good, adding to temp list")
+                        tvallist.append(reformattedprefixrange)
+                elif ipaddress.ip_network(basenetwork).version == 6:
+                    # print("got valid v6 for the base network")
+                    if ipaddress.ip_network(
+                            maxlennetwork).version == 6:  # checking that the prefix with max length mask is valid
+                        # print("max len v4 is good, adding to temp list")
+                        # need to reformat the v6 address to remove any leading zeros
+                        reformattedv6rangeprefix = (
+                                    str(ipaddress.ip_network(basenetwork)) + '-' + components[2]).upper()
+                        tvallist.append(reformattedv6rangeprefix)
+            except ValueError:
+                # print("neither v4 nor v6 for the base network")
+                invalidlist.append(reformattedprefixrange)
+    print("number of valid entries: {}".format(len(tvallist)))
+    print("number of invalid entries under '[Prefixes]': {}".format(len(invalidlist)))
+    for badprefix in invalidlist:
+        print(badprefix)
+    return tvallist
 
-
+"""
 def validatePrefixList(pfList):
     '''
     validate the list of prefixes
-    normalize IPv6 to compressed form (because ARIN's parser currently doesn't support uncompressed v6 addrs)
+    return list of valid and invalid prefixes
     count the number of errors (if any)
     :param pfList:
     :return: valids, invalidPFlist
@@ -73,13 +121,11 @@ def validatePrefixList(pfList):
         try:
             ipaddress.ip_network(item)
             validPrefixes += 1
-            if item.upper() != str(ipaddress.ip_network(item)).upper():
-                pfList[index] = str(ipaddress.ip_network(item)).upper()
         except ValueError:
             invalidPFlist.append(item)
     valids = set(pfList) - set(invalidPFlist)
     return valids, invalidPFlist
-
+"""
 
 def generateROAreqLine(inDict):
     '''
@@ -109,11 +155,11 @@ def generateROAreqLine(inDict):
     roareq = "{}|{}|{}|{}|{}|{}|".format(inDict['Version'], epochtime, inDict['ROAName'], inDict['OriginAS'],
                                          inDict['StartDate'], inDict['EndDate'])
     for entry in inDict['Prefixes']:
-        components = re.split("[/ -]", entry)
+        components = re.split("[/-]", entry)
         if len(components) == 2:                                # no mask length option
             roareq += '{}|{}||'.format(components[0], components[1])
         else:                                                   # optional masklength
-            roareq += '{}|{}|{}|'.format(components[0], components[1], components[4])
+            roareq += '{}|{}|{}|'.format(components[0], components[1], components[2])
     return roareq
 
 
@@ -162,7 +208,7 @@ def createSignedRequest(roaReqLine, ROAName, privKey):
 def main():
     allInfo = readYML()
     # preprocess prefixes to catch lines with max length
-    preProcessPrefixes(allInfo['Prefixes'])
+    allInfo['Prefixes'] = preProcessPrefixes(allInfo['Prefixes'])
     roaRequstData = generateROAreqLine(allInfo)
     createSignedRequest(roaRequstData, allInfo['ROAName'], allInfo['Keyfile'])
 
