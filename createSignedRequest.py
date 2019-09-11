616714897ef6052b34b7b71e528b0000045fa2ec
Andrew Gallo <agallo@gwu.edu>
2019-09-11 12:55:49 -0400

    additional error handling added

--- createSignedRequest.py
+++ createSignedRequest.py
@@ -8,7 +8,6 @@ import subprocess
 import time
 import yaml
 
-
 parser = ArgumentParser(description="Generate signed ROA requests for ARIN portal")
 
 parser.add_argument("-r", "--ROAinfo", dest="r", metavar="",
@@ -24,27 +23,28 @@ ROAinfoFile = args.r
 def readYML():
     """
     read YML file to gather info about ROAs to create
-    :return: ROAdata (dict, keys:ROAName, Prefix, Version, OriginAS, EndDate, StartDate, Keyfile)
+    :return: ROAinfo (dict, keys:ROAName, Prefix, Version, OriginAS, EndDate, StartDate, Keyfile)
     """
     with open(ROAinfoFile, 'r') as infile:
-        rinfo = yaml.load(infile)
-    return rinfo
+        ROAinfo = yaml.load(infile)
+    return ROAinfo
 
 
 def preProcessPrefixes(prefixList):
     """
     preprocess the prefix list.
-    because ROAs allow option max length on a prefix (to allow for a range of mask values), this function
-    will detect if that option is in use and validate both the shortest and longest prefixes to ensure
-    they're valid.
-    This temporary list will be run through 'validatePrefixList' to ensure we're working with valid data
-    in the event that there are *any* invalids, they'll be noted, and the whole script will bail
+    this function examines the list of prefixes provided in the YML file.  It checks to see if
+    we have a valid v4 or v6 address.
+    For entries that use the optional mask length, validate both the shortest and longest prefixes to catch errors
+    such as 172.16.0.0/16-44 (with 44 being an invalid mask length for v4)
+    Additionally, for v6, we'll reformat the address to the compressed version by removing leading spaces in any section.
+    For example, 2001:2001:0DB8::/32 will become 2001:DB8::/32 because ARIN's portal (currently) doesn't accept
+    uncompressed v6 addresses
     :param prefixList:
     :return: processedPrefixList
     """
     tvallist = []
     invalidlist = []
-    print("Entering preProcessPrefixes with prefixList = {}".format(prefixList))
     for index, item in enumerate(prefixList):
         # remove any whitespace that may be in the entry, can happen especially when mask range is in use
         # this makes the result of split more predictable
@@ -56,76 +56,50 @@ def preProcessPrefixes(prefixList):
             # an entry not using the mask range option, 'item' should be good enough, but rebuilding from the
             # components drives the point home
             reformattedprefix = components[0] + '/' + components[1]
-            # print("{} {} no mask range".format(index, reformattedprefix))
             try:
                 if ipaddress.ip_network(reformattedprefix).version == 4:  # we have a valid v4 prefix
-                    # print("got valid v4")
                     tvallist.append(reformattedprefix)
                 elif ipaddress.ip_network(reformattedprefix).version == 6:  # we have a valid v6 prefix
-                    # print("got valid v6")
                     # need to use the output of ipaddress.ip_network because that compresses the v6 addr
                     # (removes leading zeros), which the ARIN portal doesn't understand
                     tvallist.append(str(ipaddress.ip_network(reformattedprefix)).upper())
             except ValueError:  # what ever entry is, it isn't valid
-                # print("neither v4 nor v6")
                 invalidlist.append(item)
         # this section is a bit more complicated- for networks where the mask range option is used
         # eg: 172.16.0.0/16-18, we need to temporarily deconstruct the entry, ensure that both the
         # base network (in this example 172.16.0.0/16) AND the prefix with the max length
         # (in this example 172.16.0.0/18) are both valid.
         # we then have to reconstruct the string from the validated components (especially in the case of v6, where
-        # leading zeros need to be removed)
+        # leading zeros need to be removed).  This is what gets added to the temporary valid list, which
+        # ultimately replaces the dictionary (key: Prefixes) that was originally read from the YML file
         if len(components) == 3:  # range option being used, we have address, base mask + max length
-            # print("{} {} using mask range".format(index, item))
             basenetwork = components[0] + '/' + components[1]
             maxlennetwork = components[0] + '/' + components[2]
             reformattedprefixrange = components[0] + '/' + components[1] + '-' + components[2]
             # first, validate the base network
             try:
                 if ipaddress.ip_network(basenetwork).version == 4:
-                    # print("got valid v4")
                     if ipaddress.ip_network(
                             maxlennetwork).version == 4:  # checking that the prefix with max length mask is valid
-                        # print("max len v4 is good, adding to temp list")
                         tvallist.append(reformattedprefixrange)
                 elif ipaddress.ip_network(basenetwork).version == 6:
-                    # print("got valid v6 for the base network")
                     if ipaddress.ip_network(
                             maxlennetwork).version == 6:  # checking that the prefix with max length mask is valid
-                        # print("max len v4 is good, adding to temp list")
                         # need to reformat the v6 address to remove any leading zeros
                         reformattedv6rangeprefix = (
-                                    str(ipaddress.ip_network(basenetwork)) + '-' + components[2]).upper()
+                                str(ipaddress.ip_network(basenetwork)) + '-' + components[2]).upper()
                         tvallist.append(reformattedv6rangeprefix)
             except ValueError:
-                # print("neither v4 nor v6 for the base network")
                 invalidlist.append(reformattedprefixrange)
-    print("number of valid entries: {}".format(len(tvallist)))
-    print("number of invalid entries under '[Prefixes]': {}".format(len(invalidlist)))
+    print("\nNumber of valid entries included in ROA request: {}".format(len(tvallist)))
+    print("\nNumber of invalid entries under '[Prefixes]': {}".format(len(invalidlist)))
+    print(
+        "The following have been EXCLUDED from the signed ROA request.  If you want them included in the same ROA,"
+        " please edit the input YML to correct the error and re-run.")
     for badprefix in invalidlist:
         print(badprefix)
     return tvallist
 
-"""
-def validatePrefixList(pfList):
-    '''
-    validate the list of prefixes
-    return list of valid and invalid prefixes
-    count the number of errors (if any)
-    :param pfList:
-    :return: valids, invalidPFlist
-    '''
-    validPrefixes = 0
-    invalidPFlist = []
-    for index, item in enumerate(pfList):
-        try:
-            ipaddress.ip_network(item)
-            validPrefixes += 1
-        except ValueError:
-            invalidPFlist.append(item)
-    valids = set(pfList) - set(invalidPFlist)
-    return valids, invalidPFlist
-"""
 
 def generateROAreqLine(inDict):
     '''
@@ -156,9 +130,9 @@ def generateROAreqLine(inDict):
                                          inDict['StartDate'], inDict['EndDate'])
     for entry in inDict['Prefixes']:
         components = re.split("[/-]", entry)
-        if len(components) == 2:                                # no mask length option
+        if len(components) == 2:  # no mask length option
             roareq += '{}|{}||'.format(components[0], components[1])
-        else:                                                   # optional masklength
+        else:  # optional masklength
             roareq += '{}|{}|{}|'.format(components[0], components[1], components[2])
     return roareq
 
@@ -175,8 +149,9 @@ def createSignedRequest(roaReqLine, ROAName, privKey):
     # write the ROA request line to a file so we can sign it
     with open('roadata.txt', 'w') as roadata:
         roadata.write(roaReqLine)
-    #    roadata.write('\n')
     # sign the ROA request line and convert the signature format
+    # if the private key can't be read or is invalid, openssl with throw an error, so we need to catch it
+    # and exit as gracefully as possible
     try:
         subprocess.check_output(["openssl", "dgst", "-sha256", "-sign", privKey, "-keyform", "PEM", "-out",
                                  "signature", "roadata.txt"])
@@ -207,7 +182,6 @@ def createSignedRequest(roaReqLine, ROAName, privKey):
 
 def main():
     allInfo = readYML()
-    # preprocess prefixes to catch lines with max length
     allInfo['Prefixes'] = preProcessPrefixes(allInfo['Prefixes'])
     roaRequstData = generateROAreqLine(allInfo)
     createSignedRequest(roaRequstData, allInfo['ROAName'], allInfo['Keyfile'])
